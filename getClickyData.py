e8d7d74bd6729d95ae29ab7b6648d47fb295e750
Russell Parker <russell.parker7@gmail.com>
2015-06-18 14:43:08 -0400

    Reordered functions

--- getClickyData.py
+++ getClickyData.py
@@ -13,29 +13,6 @@ except ImportError:
 	
 import config
 
-def getMonths(beginDate, endDate):
-	startMonth = beginDate.month
-	endMonths  = (endDate.year-beginDate.year)*12 + endDate.month+1
-	return [datetime.strftime(datetime(year=yr, month=mn, day=1), '%Y-%m') for (yr, mn) in (
-			((m - 1) / 12 + dt1.year, (m - 1) % 12 + 1) for m in range(start_month, end_months)
-			)]
-
-def getClickyData(month):
-	# For more info: http://clicky.com/help/api
-	requested = urllib2.Request('http://api.clicky.com/api/stats/4?' + 
-		'site_id=' + config.site_id + '&sitekey=' + config.sitekey + 
-		'&type=visitors,actions&date=' + month +'&output=xml')
-	try:
-		response = urllib2.urlopen( requested )
-		return parseClickyData(response)
-	except urllib2.HTTPError as error:
-		print 'The server couldn\'t fulfill the request.'
-		print 'Error code: ', error.code
-		return ''
-	except urllib2.URLError as error:
-		print 'Failed to reach a server.'
-		print 'Reason: ', error.reason
-		return ''
 
 # Given the user-provided xml doc, returns an elementTree object of JoMI's
 # daily visitors and actions stats during the specified time period from Clicky
@@ -61,10 +38,32 @@ def parseRequest(info):
 	# Date range request format: YYYY-MM-DD,YYYY-MM-DD, e.g. 2015-05-01,2015-05-31
 	# According to API:
 		# The maximum range is 750 days for Pro users, and 31 days for everyone else.
-		
-	
 	return xmlContent
 
+
+def getMonths(beginDate, endDate):
+	startMonth = beginDate.month
+	endMonths  = (endDate.year-beginDate.year)*12 + endDate.month+1
+	return [datetime.strftime(datetime(year=yr, month=mn, day=1), '%Y-%m') for (yr, mn) in (
+			((m - 1) / 12 + dt1.year, (m - 1) % 12 + 1) for m in range(start_month, end_months)
+			)]
+
+def getClickyData(month):
+	# For more info: http://clicky.com/help/api
+	requested = urllib2.Request('http://api.clicky.com/api/stats/4?' + 
+		'site_id=' + config.site_id + '&sitekey=' + config.sitekey + 
+		'&type=visitors,actions&date=' + month +'&output=xml')
+	try:
+		response = urllib2.urlopen( requested )
+		return parseClickyData(response)
+	except urllib2.HTTPError as error:
+		print 'The server couldn\'t fulfill the request.'
+		print 'Error code: ', error.code
+		return ''
+	except urllib2.URLError as error:
+		print 'Failed to reach a server.'
+		print 'Reason: ', error.reason
+		return ''
 
 
 # createTree: creates an elementTree object of an xml file and returns it
