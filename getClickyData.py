43cd3ee19429c5648d2866fc27a89701eff3ac81
digitaltembo <thatwhich@dijitalelefan.com>
2015-06-18 14:53:48 -0400

    stuff

--- getClickyData.py
+++ getClickyData.py
@@ -13,39 +13,11 @@ except ImportError:
 	
 import config
 
-
-# Given the user-provided xml doc, returns an elementTree object of JoMI's
-# daily visitors and actions stats during the specified time period from Clicky
-def parseRequest(info):
-	requestXML = createTree(info)
-	beginDate = datetime.strftime(requestXML.find('ReportDefinition/Filters/UsageDateRange/Begin').text.strip(),
-								  '%Y-%m-%d')
-	endDate   = datetime.strftime(requestXML.find('ReportDefinition/Filters/UsageDateRange/End').text.strip(),
-								  '%Y-%m-%d')
-	print beginDate, endDate
-	
-	months = getMonths(beginDate, endDate)
-	print getMonths
-	xmlContent = ''
-	#for month in months:
-		#try:
-			#with open('cache/' + month + '.xml') as monthCache:
-				#xmlContent += monthCache.read()
-		#except IOError:
-			#xmlContent += getClickyData(month)
-				
-	# return requestXML
-	# Date range request format: YYYY-MM-DD,YYYY-MM-DD, e.g. 2015-05-01,2015-05-31
-	# According to API:
-		# The maximum range is 750 days for Pro users, and 31 days for everyone else.
-	return xmlContent
-
-
 def getMonths(beginDate, endDate):
 	startMonth = beginDate.month
 	endMonths  = (endDate.year-beginDate.year)*12 + endDate.month+1
-	return [datetime.strftime(datetime(year=yr, month=mn, day=1), '%Y-%m') for (yr, mn) in (
-			((m - 1) / 12 + dt1.year, (m - 1) % 12 + 1) for m in range(start_month, end_months)
+	return [datetime(year=yr, month=mn, day=1).strftime('%Y-%m') for (yr, mn) in (
+			((m - 1) / 12 + beginDate.year, (m - 1) % 12 + 1) for m in range(startMonth, endMonths)
 			)]
 
 def getClickyData(month):
@@ -64,6 +36,35 @@ def getClickyData(month):
 		print 'Failed to reach a server.'
 		print 'Reason: ', error.reason
 		return ''
+
+# Given the user-provided xml doc, returns an elementTree object of JoMI's
+# daily visitors and actions stats during the specified time period from Clicky
+def parseRequest(info):
+	requestXML = createTree(info)
+	beginDate = datetime.strptime(requestXML.find('ReportDefinition/Filters/UsageDateRange/Begin').text.strip(),
+								  '%Y-%m-%d')
+	endDate   = datetime.strptime(requestXML.find('ReportDefinition/Filters/UsageDateRange/End').text.strip(),
+								  '%Y-%m-%d')
+	print beginDate, endDate
+	
+	months = getMonths(beginDate, endDate)
+	print months
+	xmlContent = ''
+	#for month in months:
+		#try:
+			#with open('cache/' + month + '.xml') as monthCache:
+				#xmlContent += monthCache.read()
+		#except IOError:
+			#xmlContent += getClickyData(month)
+				
+	# return requestXML
+	# Date range request format: YYYY-MM-DD,YYYY-MM-DD, e.g. 2015-05-01,2015-05-31
+	# According to API:
+		# The maximum range is 750 days for Pro users, and 31 days for everyone else.
+		
+	
+	return xmlContent
+
 
 
 # createTree: creates an elementTree object of an xml file and returns it
